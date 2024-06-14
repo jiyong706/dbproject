ca02df7d0a84f2392de82d4c58343cad9ac26c05
jiyong706 <148942813+jiyong706@users.noreply.github.com>
2024-06-14 14:24:24 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<!-- config.php -->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pilleat_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
