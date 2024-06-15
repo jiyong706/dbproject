a833959976c21f9f76a409317c7f122aefdaa3c9
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2024-06-15 16:36:07 +0900

    Update config.php

--- DB/config.php
+++ DB/config.php
@@ -1,9 +1,16 @@
 <!-- config.php -->
 <?php
+<<<<<<< Updated upstream
     $host = 'azza.gwangju.ac.kr/orcl';
     $user = 'dbuser201692';
     $pw = 'ce1234';
     $conn = oci_connect($host, $user, $pw) ; //db 연결
+=======
+    $host = 'dsapoi881.duckdns.org:1522/xe';
+    $user = 'c##test';
+    $pw = '00000000';
+    $conn = oci_connect($host, $user, $pw); //db 연결
+>>>>>>> Stashed changes
 
     // Check connection
     if ($conn==null) {
