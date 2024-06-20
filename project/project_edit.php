6648a9c1b723f7813bb2bd3e46c4e393ac8a10c2
rlaalsrl0880 <1astwallq@gmail.com>
2024-06-21 06:41:58 +0900

    project_edit.php
    getProjectDetails 제외

--- project/project_edit.php
+++ project/project_edit.php
@@ -6,34 +6,28 @@ if (!isset($_SESSION['user_nickname'])) {
 }
 
 include_once '../user/config.php';
-include_once '../DB/data_select_project.php';
 
-if (isset($_GET['project_id'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET['project_id'])) {
     $project_id = $_GET['project_id'];
+    $project_name = $_POST['project_name'];
+    $project_info = $_POST['project_info'];
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $project_name = $_POST['project_name'];
-        $project_info = $_POST['project_info'];
+    $sql = "UPDATE project SET project_name = :project_name, project_info = :project_info, project_update = SYSDATE WHERE project_id = :project_id";
+    $stid = oci_parse($conn, $sql);
+    oci_bind_by_name($stid, ':project_id', $project_id);
+    oci_bind_by_name($stid, ':project_name', $project_name);
+    oci_bind_by_name($stid, ':project_info', $project_info);
 
-        $sql = "UPDATE project SET project_name = :project_name, project_info = :project_info, project_update = SYSDATE WHERE project_id = :project_id";
-        $stid = oci_parse($conn, $sql);
-        oci_bind_by_name($stid, ':project_id', $project_id);
-        oci_bind_by_name($stid, ':project_name', $project_name);
-        oci_bind_by_name($stid, ':project_info', $project_info);
-
-        if (oci_execute($stid)) {
-            echo "<script>alert('프로젝트가 수정되었습니다.'); location.replace('project_list.php');</script>";
-        } else {
-            $e = oci_error($stid);
-            echo "프로젝트 수정 오류: " . $e['message'];
-        }
-
-        oci_free_statement($stid);
-        oci_close($conn);
+    if (oci_execute($stid)) {
+        echo "<script>alert('프로젝트가 수정되었습니다.'); location.replace('project_list.php');</script>";
     } else {
-        $project = getProjectDetails($project_id);
+        $e = oci_error($stid);
+        echo "프로젝트 수정 오류: " . $e['message'];
     }
-} else {
+
+    oci_free_statement($stid);
+    oci_close($conn);
+} elseif (!isset($_GET['project_id'])) {
     echo "<script>alert('유효하지 않은 요청입니다.'); location.replace('project_list.php');</script>";
 }
 ?>
@@ -51,17 +45,18 @@ if (isset($_GET['project_id'])) {
         <div class="form-header">
             <h1>프로젝트 수정</h1>
         </div>
-        <form method="post" action="project_edit.php?project_id=<?php echo $project_id; ?>">
+        <form method="post" action="project_edit.php?project_id=<?php echo isset($_GET['project_id']) ? $_GET['project_id'] : ''; ?>">
             <div class="form-group">
                 <label for="project_name">프로젝트 이름:</label>
-                <input type="text" id="project_name" name="project_name" value="<?php echo htmlspecialchars($project['PROJECT_NAME']); ?>" required>
+                <input type="text" id="project_name" name="project_name" value="" required>
             </div>
             <div class="form-group">
                 <label for="project_info">프로젝트 설명:</label>
-                <textarea id="project_info" name="project_info" required><?php echo htmlspecialchars($project['PROJECT_INFO']); ?></textarea>
+                <textarea id="project_info" name="project_info" required></textarea>
             </div>
             <button type="submit">프로젝트 수정</button>
         </form>
     </div>
 </body>
 </html>
+
