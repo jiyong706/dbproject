463cb5b0693022fdbf22234b6cf89206587e0a9d
fghjklvmbn <dsapoi881@gmail.com>
2024-06-21 16:46:06 +0900

    패널 폼 수정

--- project/pannel/pannel_form.php
+++ project/pannel/pannel_form.php
@@ -11,35 +11,67 @@
         <div class="survey-header">
             <h1>이지패널 설문지</h1>
             <hr>
-        <!--여기서부터 php 활용-->
-            <?php
-             
-            ?>
-    
             <p>설문지 설명</p>
         </div>
         <form method="post" action="submit.php">
+            <?php
+                if (isset($_POST['add_question'])) {
+                    $questions = $_POST['questions'];
+                } else {
+                    $questions = [];
+                }
+
+                // 기존 질문을 표시
+                foreach ($questions as $index => $question) {
+                    echo '<div class="question-container">';
+                    echo '<div class="question-header">';
+                    echo '<input type="text" name="questions[' . $index . '][title]" value="' . $question['title'] . '" placeholder="제목없는 질문">';
+                    echo '<select name="questions[' . $index . '][type]">';
+                    echo '<option value="multiple_choice"'.($question['type'] == 'multiple_choice' ? ' selected' : '').'>객관식 질문</option>';
+                    echo '<option value="subjective"'.($question['type'] == 'subjective' ? ' selected' : '').'>주관식 질문</option>';
+                    echo '</select>';
+                    echo '</div>';
+
+                    if ($question['type'] == 'multiple_choice') {
+                        echo '<div class="options">';
+                        foreach ($question['options'] as $optIndex => $option) {
+                            echo '<div class="option">';
+                            echo '<input type="radio" name="questions[' . $index . '][option]" disabled>';
+                            echo '<input type="text" name="questions[' . $index . '][options][]" value="' . $option . '" placeholder="옵션">';
+                            echo '</div>';
+                        }
+                        echo '<div class="option">';
+                        echo '<input type="radio" name="questions[' . $index . '][option]" disabled>';
+                        echo '<input type="text" name="questions[' . $index . '][options][]" placeholder="옵션 추가 또는 \'기타\' 추가">';
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                }
+            ?>
+
+            <!-- 새로운 질문 폼을 추가하기 위한 부분 -->
             <div class="question-container">
                 <div class="question-header">
-                    <input type="text" name="question_title" placeholder="제목없는 질문">
-                    <select name="question_type">
+                    <input type="text" name="questions[new][title]" placeholder="제목없는 질문">
+                    <select name="questions[new][type]">
                         <option value="multiple_choice">객관식 질문</option>
-                        <option value="multiple_choice">주관식 질문</option>
-                        <!-- 다른 질문 유형을 추가할 수 있습니다 -->
+                        <option value="subjective">주관식 질문</option>
                     </select>
                 </div>
                 <div class="options">
                     <div class="option">
                         <input type="radio" name="option" disabled>
-                        <input type="text" name="option_text[]" placeholder="옵션 1">
+                        <input type="text" name="questions[new][options][]" placeholder="옵션 1">
                     </div>
                     <div class="option">
                         <input type="radio" name="option" disabled>
-                        <input type="text" name="option_text[]" placeholder="옵션 추가 또는 '기타' 추가">
+                        <input type="text" name="questions[new][options][]" placeholder="옵션 추가 또는 '기타' 추가">
                     </div>
                 </div>
             </div>
-            <button type="submit">제출</button>
+            <button type="submit" name="add_question">질문 추가</button>
+            <button type="submit" formaction="submit.php">제출</button>
         </form>
     </div>
 </body>
